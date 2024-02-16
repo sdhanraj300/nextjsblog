46cc5bb17e03cf5319e730115345fcac4f5d07a7
Dhanraj Singh <sdhanraj300@gmail.com>
2024-02-17 00:03:27 +0530

    completed category list responsiveness

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,7 +1,93 @@
 import React from "react";
 import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 const CategoryList = () => {
-  return <div className={styles.container}>CategoryList</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/style.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Style
+        </Link>
+        <Link
+          href="/blog?cat=fashion"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/fashion.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Fashion
+        </Link>
+        <Link
+          href="/blog?cat=travel"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/travel.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Travel
+        </Link>
+        <Link
+          href="/blog?cat=food"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/food.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Food
+        </Link>
+        <Link
+          href="/blog?cat=culture"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/culture.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Culture
+        </Link>
+        <Link
+          href="/blog?cat=coding"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/coding.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Coding
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryList;
